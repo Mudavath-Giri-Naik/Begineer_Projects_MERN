f6cf59a0471515bd510532a7ac6e3589d04b6a99
Mudavath-Giri-Naik <mudavathgirinaik444@gmail.com>
2025-01-24 19:46:15 +0530

    Now the databse is connected

--- TodoManageAPI/backend/server.js
+++ TodoManageAPI/backend/server.js
@@ -1,25 +1,38 @@
-
+require('dotenv').config();
 const express = require('express');
-//const connectDB = require('./config/database');
+const connectDB = require('./config/database');
 const taskRouter = require('./routes/tasks');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-//connectDB();
-
-app.use(express.json());  // To parse JSON request bodies
+app.use(express.json());
 app.use('/tasks', taskRouter);
 
-// Add a route for the root path ("/")
 app.get('/', (req, res) => {
-  res.send(`
+    res.status(200).send(`
     <h1>Welcome!</h1>
     <p>The server is running. Access task-related functionalities through the <a href="/tasks">/tasks</a> endpoint.</p>
     `);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log(`Open your browser and navigate to: http://localhost:${port}`); // Added line
-});
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!'); // Or a more detailed message
+  });
+
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+      console.log(`Open your browser and navigate to: http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1); // Terminate application if DB connection fails
+  }
+}
+//this line will start server
+startServer();
